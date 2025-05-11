8e8a1e2caa7b479019ba820cfe6f31de0a41cedf
LinG <w93163red@gmail.com>
2025-05-11 00:26:58 -0400

    hello

--- bpf-progs/empty.btf.user.c
+++ bpf-progs/empty.btf.user.c
@@ -6,9 +6,6 @@
 
 static int libbpf_print_fn(enum libbpf_print_level level, const char *format,
                            va_list args) {
-  if (level >= LIBBPF_DEBUG)
-    return 0;
-
   return vfprintf(stderr, format, args);
 }
 
--- bpf-progs/empty.kern.c
+++ bpf-progs/empty.kern.c
@@ -6,11 +6,12 @@
 
 #define __contains(name, node) __attribute__((btf_decl_tag("contains:" #name ":" #node)))
 
-extern void *bpf_obj_new_impl(__u64 local_type_id, void *meta) __ksym;
 #define bpf_obj_new(type)                                                      \
   ((type *)bpf_obj_new_impl(bpf_core_type_id_local(type), NULL))
-extern void bpf_obj_drop_impl(void *kptr, void *meta) __ksym;
+extern void *bpf_obj_new_impl(u64 local_type_id__k, void *meta__ign) __weak __ksym;
+
 #define bpf_obj_drop(kptr) bpf_obj_drop_impl(kptr, NULL)
+extern void bpf_obj_drop_impl(void *p__alloc, void *meta__ign) __weak __ksym;
 
 char _license[] SEC("license") = "GPL";
 
