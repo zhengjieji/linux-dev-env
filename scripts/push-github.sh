6cfa9f5db5f1935b65ff295c7b04ba71a613c76d
zhengjieji <realruoji@gmail.com>
2026-02-24 02:15:48 +0000

    chore: update dual-vm

--- scripts/push-github.sh
+++ scripts/push-github.sh
@@ -6,7 +6,7 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="$(cd -- "${SCRIPT_DIR}/.." && pwd)"
 
 REMOTE="origin"
-BRANCH="dual-vm"
+BRANCH="katran-exp"
 COMMIT_MESSAGE="chore: update dual-vm"
 FORCE_WITH_LEASE=0
 
